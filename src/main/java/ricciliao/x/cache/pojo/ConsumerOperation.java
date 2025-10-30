7fb4915a9fd076c85624dbd2054942e8dd9a3073
ricciliao <riccix@163.com>
2025-10-30 10:34:45 +0800

    refactor: upgrade cache framework.

--- src/main/java/ricciliao/x/cache/pojo/ConsumerOperation.java
+++ src/main/java/ricciliao/x/cache/pojo/ConsumerOperation.java
@@ -3,7 +3,8 @@ package ricciliao.x.cache.pojo;
 import ricciliao.x.cache.ConsumerStore;
 
 import java.io.Serial;
-import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntFunction;
 
 public interface ConsumerOperation {
 
@@ -12,22 +13,22 @@ public interface ConsumerOperation {
         return new ConsumerOperation.Single<>(single);
     }
 
-    static <T extends ConsumerStore> Single<T> of(T store) {
+    static <T extends ConsumerStore> Single<T> ofStore(T store) {
 
         return new ConsumerOperation.Single<>(new ConsumerCache<>(store));
     }
 
-    static <T extends ConsumerStore> Batch<T> of(ConsumerCache<T>[] batch) {
+    static <T extends ConsumerStore> Batch<T> of(List<ConsumerCache<T>> caches) {
 
-        return new ConsumerOperation.Batch<>(batch);
+        return new ConsumerOperation.Batch<>(caches.toArray((IntFunction<ConsumerCache<T>[]>) ConsumerCache[]::new));
     }
 
-    static <T extends ConsumerStore> Batch<T> of(T[] stores) {
+    static <T extends ConsumerStore> Batch<T> ofStore(List<T> stores) {
 
         return new ConsumerOperation.Batch<>(
-                Arrays.stream(stores)
+                stores.stream()
                         .map(ConsumerCache::new)
-                        .toArray(ConsumerCache[]::new)
+                        .toArray((IntFunction<ConsumerCache<T>[]>) ConsumerCache[]::new)
         );
     }
 
